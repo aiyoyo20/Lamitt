7a21a4b67df84d629c1aa4006003c101901f188a
aiyoyo20 <aiyoyo20@gmail.com>
2023-04-11 06:43:41 -0400

    create project configuration file

---
+++ Lamitt/config.py
@@ -0,0 +1,16 @@
+redis_config = {
+        'host': 'localhost',
+        'port': 6379,
+        'db': 0,
+}
+
+blacklist = {
+        "ua_key": "ua_blacklist",
+        "ip_key": "ip_blacklist",
+        "userid_key": "userid_blacklist"
+}
+
+
+video_config = {
+        "video_path": "/run/media/aiyoyo/fd4a92ae-fdb7-4744-86f0-ed977d4da971/video/videos/"
+}
