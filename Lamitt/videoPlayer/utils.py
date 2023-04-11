28de0fa28a42af0243018abe501809a97b3af33e
aiyoyo20 <aiyoyo20@gmail.com>
2023-04-11 07:17:05 -0400

    add get_video_files function docstring

--- Lamitt/videoPlayer/utils.py
+++ Lamitt/videoPlayer/utils.py
@@ -8,5 +8,8 @@ SUPPORTED_FORMATS = [".mp4", ".avi", ".mkv"]
 
 
 def get_video_files():
+    '''获取目标地址下的所有视频文件
+    '''
+
     video_files = [f for f in os.listdir(VIDEO_PATH) if f.lower().endswith(tuple(SUPPORTED_FORMATS))]
     return video_files
